3ce03d3ab03bfbd2e7016084539bf8e9c70ef8c2
sabari245 <sabari2004@hotmail.com>
2022-09-22 17:04:40 +0530

    patient page -> remaining: records, details, recent; onchain code integration remaining

---
+++ components/patient/config.js
@@ -0,0 +1,21 @@
+const links = [
+    {
+        name: "Dashboard",
+        image: "/assets/dashboard.png",
+        link: "/patient"
+    },
+    {
+      name: "Records",
+      image: "/assets/document.png",
+      link: "/patient/records"
+    }
+  ]
+  
+
+  const testPatientDetails = {
+    adhaar: "123456789012",
+    name: "John Doe",
+    dob: "12/12/2021",
+    phone: "1234567890",
+  }
+  export { links, testPatientDetails }
